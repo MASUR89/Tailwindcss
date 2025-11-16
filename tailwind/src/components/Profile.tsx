f0515875e9bac5d26c26bd84a0a058390c71d7d7
MASUR89 <surmava9@gmail.com>
2025-11-16 14:25:39 +0400

    added responsive

--- tailwind/src/components/Profile.tsx
+++ tailwind/src/components/Profile.tsx
@@ -5,6 +5,7 @@ import StudyIcon from "../assets/images/study.svg";
 import ExerciseIcon from "../assets/images/exercise.svg";
 import SocialIcon from "../assets/images/social.svg";
 import SelfCareIcon from "../assets/images/selfcare.svg";
+import ProfileIcon from "../assets/images/Jeremy.png";
 
 
 const Profile = () => {
@@ -18,27 +19,35 @@ const Profile = () => {
   ];
 
   return (
-    <div className="w-screen h-screen bg-[#0f1424] flex items-center justify-center p-4">
-      <div className="w-full max-w-[1080px] flex flex-col md:flex-row items-start gap-6">
+    <div className="w-full min-h-screen bg-[#0f1424] flex items-center justify-center p-4">
+      <div className="w-full max-w-[1080px] flex flex-col md:flex-row items-center gap-6">
 
         {/* LEFT PROFILE CARD */}
-        <div className="w-full md:w-[255px] bg-[#1c204b] rounded-[25px] flex flex-col">
-          <div className="bg-[#5747EA] h-[354px] rounded-[25px] p-10 flex flex-col">
-            <div className="w-[78px] h-[78px] border border-black rounded-full bg-white"></div>
-            <div className="mt-10 text-left text-[#bbc0ff]">
-              <h3>Report for</h3>
-              <h1 className="text-[36px] leading-11 font-light text-white">Jeremy Robson</h1>
+        <div className="w-[255px] md:w-[255px] bg-[#1c204b] rounded-[25px] flex flex-col 
+          max-[430px]:w-[328px] max-[430px]:h-[203px]
+          max-[768px]:w-[255px ]">
+          <div className="bg-[#5747EA] h-[354px] w-full rounded-[25px] p-10 flex flex-col
+            max-[430px]:h-[133px] max-[430px]:flex-row max-[430px]:pl-7">
+            <div className="w-20 h-20 rounded-full 
+              max-[430px]:w-[68px] max-[430px]:h-[68px] ">
+              <img src={ProfileIcon} alt="Jeremy" className="w-full h-full object-cover" />
+            </div>
+            <div className="mt-10 text-left text-[#bbc0ff]
+              max-[430px]:mt-1 max-[430px]:ml-4">
+              <h3 className="max-[430px]:text-[15px]">Report for</h3>
+              <h1 className="text-[36px] leading-11 font-light text-white
+              max-[430px]:text-[24px] max-[430px]:leading-5">Jeremy Robson</h1>
             </div>
           </div>
-          <div className="p-8 text-left">
-            <h3 className="text-[#bbc0ff] mt-1">Daily</h3>
-            <h3 className="text-white mt-3">Weekly</h3>
-            <h3 className="text-[#bbc0ff] mt-3">Monthly</h3>
+          <div className="p-8 text-left max-[430px]:flex max-[430px]:justify-around max-[430px]:p-4">
+            <div className="text-[#7078c9] mt-1 text-[18px] max-[430px]:mt-3 cursor-pointer hover:text-white transition-colors duration-300">Daily</div>
+            <div className="text-[#7078c9] mt-3 text-[18px] cursor-pointer hover:text-white transition-colors duration-300">Weekly</div>
+            <div className="text-[#7078c9] mt-3 text-[18px] cursor-pointer hover:text-white transition-colors duration-300">Monthly</div>
           </div>
         </div>
 
         {/* RIGHT GRID */}
-        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
           {data.map((item) => (
             <Report
               key={item.title}
--- tailwind/src/components/Report.tsx
+++ tailwind/src/components/Report.tsx
@@ -8,25 +8,24 @@ type ReportProps = {
 
 const Report = ({ title, hours, lastWeekHours, color, icon }: ReportProps) => {
   return (
-    <div className="w-[255px] h-[245px] rounded-[20px] overflow-hidden relative">
+    <div className="w-[255px] h-[245px] rounded-[20px] overflow-hidden relative max-[430px]:w-[328px] max-[430px]:h-40">
       
-      {/* ზედა ფერი + icon */}
-      <div className={`${color} w-full h-[160px] flex justify-end items-start pr-4 text-3xl absolute top-0 z-0`}>
+      <div className={`${color} w-full h-40 flex justify-end items-start pr-4 ml-0.5 text-3xl absolute top-0 z-0 max-[430px]:h-30`}>
          <img src={icon} alt={title} className="w-[78px] h-[78px] opacity-90 absolute -top-[5px]" />
       </div>
 
-      {/* ქვედა მუქი ბლოკი */}
-      <div className="bg-[#1c204b] w-full h-[200px] p-5 rounded-[20px] flex flex-col justify-between absolute bottom-0 z-1">
+      <div className="bg-[#1c204b] w-full h-[200px] p-5 rounded-[20px] flex flex-col justify-between absolute bottom-0 left-0 z-1 
+        max-[430px]:h-[123px] cursor-pointer hover:bg-[#33397a] transition-colors duration-300">
         <div className="flex justify-between items-center">
           <h3 className="text-white">{title}</h3>
           <span className="text-[#bbc0ff] text-xl">•••</span>
         </div>
 
-        <div>
-          <p className="text-white text-4xl font-light">{hours}hrs</p>
-          <p className="text-[#bbc0ff] text-sm mt-1">
+        <div className="mb-2 max-[430px]:flex max-[430px]:flex-row max-[430px]:justify-between max-[430px]:items-center">
+          <div className="text-white text-[56px] font-light max-[430px]:text-[32px]">{hours}hrs</div>
+          <div className="text-[#bbc0ff] text-[15px] mt-1">
             Last Week - {lastWeekHours}hrs
-          </p>
+          </div>
         </div>
       </div>
     </div>
