dbd8270038a6e08a8bc96318e1d035d5bf5e56e2
MASUR89 <surmava9@gmail.com>
2025-11-17 13:24:38 +0400

    svg file spelling

--- tailwind/src/components/Profile.tsx
+++ tailwind/src/components/Profile.tsx
@@ -45,7 +45,6 @@ const Profile = () => {
           </div>
         </div>
 
-        
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
           {data.map((item) => (
             <Report
